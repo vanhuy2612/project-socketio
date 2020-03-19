61c5ab67b0753d6071c44a5cbe3b62c2c925e0b1
Dinh Van Huy <dinh.van.huy@mq.local>
2020-03-19 17:37:38 +0700

    chat doi

--- server.js
+++ server.js
@@ -9,28 +9,73 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 //Loads the handlebars module
-var hbs = require('express-hbs');
 
-// Use `.hbs` for extensions and find partials in `views/partials`.
-app.engine('hbs', hbs.express4({
-  partialsDir: __dirname + '/views/partials'
-}));
-app.set('view engine', 'hbs');
-app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.set('views', path.join( __dirname, 'views' ) );
 // view engine setup
 
 app.use(express.static('public'))
 // Router : 
-app.use('/', require('./routes/index'))
+app.use(require('./routes/index'))
 
 let server = require('http').createServer(app);
-
+/*
+  Code socket io --------------------------------------------------------
+*/
 const io = require('socket.io')(server);
 
+let users = [];
+let rooms = [];
+function checkNameRoomIsExist(rooms, nameroom){
+  let ok = false;
+  rooms.forEach(room => {
+    if(room.nameroom==nameroom) ok = true;
+  });
+  return ok;
+}
 io.on('connection', socket => {
-    
+    socket.on('client-login', (data) => {
+      let user = data;
+      users.push(user);
+      io.emit('list-user', users);
+    })
+    // client join to room
+    socket.on('join-room', (data) => {
+      socket.join(data.nameroom);
+      // check nameroom in rooms
+      if(!checkNameRoomIsExist(rooms, data.nameroom)) rooms.push({
+        nameroom: data.nameroom,
+        users: []
+      });
+      rooms.forEach(room => {
+        if(room.nameroom==data.nameroom) {
+          room.users.push(data.sender);
+          io.to(data.nameroom).emit('list-user-of-room',room.users);
+          console.log(room.users);
+        }
+          
+      });
+      
+    })
+    //socket.on('person-to-room', )
+    socket.on('person-to-all', (data) => {
+      io.emit('server-send-person-to-all', (data))
+    })
+    socket.on('disconnect', () => {
+      for(let i=0; i< users.length; i++) {
+        if(socket.id == users[i].id) {
+          users.splice(i, 1);
+          io.emit('list-user', users);
+        }
+      }
+    })
 })
 
+
+
+/*
+  Finish code socket -------------------------------------------------------
+*/
 server.listen(3000, () => {
     console.log("Server is running at port 3000");
 })
